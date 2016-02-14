08ad7489ae802e60c65761af12956c7e22244994
lorenz <lorenz.rosenthal@gmail.com>
2016-02-14 14:31:28 +0100

    basic_layout: further improvements of the header and the maintable.

--- app/containers/Header.js
+++ app/containers/Header.js
@@ -4,16 +4,32 @@ class Header extends React.Component {
 	render() {
 		return (
 			<header>
-				<button><em>+</em>neu</button>
 
-				<ul id="tabbar">
-					<li>Alle</li><li>Bezahlt</li><li>Unbezahlt</li>
-				</ul>
+				<button id="main__add-btn"><em>+</em>neu</button>
 
-				<div id="pagination">
-					<span>0 - 50 von 1200</span><button className="ico-next"></button><button className="ico-prev"></button>
+				<div id="main__controls-container">
+					<ul id="tabbar">
+						<li>Alle</li><li>Bezahlt</li><li>Unbezahlt</li>
+					</ul>
+
+					<div id="pagination">
+						<span>0 - 50 von 1200</span><button className="ico-next"></button><button className="ico-prev"></button>
+					</div>
 				</div>
-
+				<table id="maintable__head">
+					<colgroup>
+						<col width="50%" />
+						<col width="50%" />
+					</colgroup>
+					<thead>
+						<tr class="actions">
+							<th></th>
+						</tr>
+						<tr class="col-titles">
+							<th>Titel</th><th>Preis</th>
+						</tr>
+					</thead>
+				</table>
 			</header>
 		)
 	}
--- app/containers/Main.js
+++ app/containers/Main.js
@@ -4,19 +4,11 @@ class Main extends React.Component {
 	render() {
 		return (
 			<main>
-				<table id="maintable">
+				<table id="maintable__body">
 					<colgroup>
 						<col width="50%" />
 						<col width="50%" />
 					</colgroup>
-					<thead>
-						<tr id="maintable__actions">
-							<th></th>
-						</tr>
-						<tr id="maintable__col-titles">
-							<th>Titel</th><th>Preis</th>
-						</tr>
-					</thead>
 					<tbody>
 						<tr>
 							<td>pupsi</td><td>dupsi</td>
