9715e39fbb78ad0b7cdeebccc29797bf5150f3e5
lorenz <lorenz.rosenthal@gmail.com>
2016-03-04 21:32:02 +0100

    Created a generic InputField component

--- app/pages/Options.js
+++ app/pages/Options.js
@@ -1,23 +1,32 @@
 import React from 'react'
 
 import HoverBox from '../components/HoverBox'
+import InputField from '../components/InputField'
+
+import mockupData from '../mockup'
 
 export default class Options extends React.Component {
 	render() {
+		let company = mockupData.options.company
 		return (
 			<main>
-				<HoverBox className="grid-col-3">
-					<h2>Firma</h2>
-				</HoverBox>
-				<HoverBox className="grid-col-3">
-					<h2>Bankdaten und Zahlungsmodalitäten</h2>
-				</HoverBox>
-				<HoverBox className="grid-col-3">
-					<h2>Allgemeines</h2>
-				</HoverBox>
-				<HoverBox className="grid-col-3">
+				<div className="row">
+					<HoverBox className="grid-col-4">
+						<h2>Firma</h2>
+						<InputField className="h2 grid-col-12" placeholder="Firmenname" text={company.name}/>
+					</HoverBox>
+					<HoverBox className="grid-col-4">
+						<h2>Bankdaten und Zahlungsmodalitäten</h2>
+					</HoverBox>
+					<HoverBox className="grid-col-4">
+						<h2>Allgemeines</h2>
+					</HoverBox>
+				</div>
+				<div className="row">
+				<HoverBox className="grid-col-4">
 					<h2>persönliche Einstellungen</h2>
 				</HoverBox>
+				</div>
 			</main>
 		)
 	}
