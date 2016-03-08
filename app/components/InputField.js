f800842def8a1e5d3e470689dcc40315bcb5228a
lorenz <lorenz.rosenthal@gmail.com>
2016-03-08 21:17:43 +0100

    InputField: freaked out a little with es6 syntax shorthands but got back on track...

--- app/components/InputField.js
+++ app/components/InputField.js
@@ -1,20 +1,30 @@
 import React from 'react'
 
-const InputField = (props) => {
-	// css-classes
-	let classNames = props.className || ''
-	let isEmpty = (value) => typeof value === 'undefined' || value === '' || value === null
+const InputField = props => {
+	
+	const isEmpty = value => typeof value === 'undefined' || value === '' || value === null
 
-	if (isEmpty(props.value)) classNames += ' empty'
+	// get css-classes
+	let classes = !!props.className ? props.className : ''
+	if (isEmpty(props.value) && isEmpty(props.children)) classes += ' empty'
 
 	// determine the input type
-	let InputType = <div className="input-field__input" contentEditable="true">{ isEmpty(props.value) ? <br /> : props.value }</div>
-	if (props.type === 'tag-container') 
-		InputType = <div className="input-field__input">{props.children}<div contentEditable="true"></div></div>
-			
+	const getInput = type => {
+		switch (type) {
+			case 'tag-container':
+				return 	<div className="input-field__input">
+							{props.children}<div contentEditable="true"></div>
+						</div> 
+			default:
+				return 	<div className="input-field__input" contentEditable="true">
+							{ isEmpty(props.value) ? <br /> : props.value }
+						</div>
+		}
+	}
+
 	return (
-		<div className={'input-field ' + classNames}>
-			{InputType}
+		<div className={ 'input-field ' + classes }>
+			{ getInput(props.type) }
 			<div className="input-field__placeholder">{props.placeholder}</div>
 		</div>
 	)
