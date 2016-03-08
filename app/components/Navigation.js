fe6c23a74d47ddfc455e723a4f5d655e8dd61ec8
lorenz <lorenz.rosenthal@gmail.com>
2016-03-08 20:24:03 +0100

    Tweaked navigation a bit.

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -3,17 +3,17 @@ import React from 'react'
 import { Link } from 'react-router'
 
 const Navigation = (props) => {
-	let page = props.page
-
-	let selected = 'js-selected ico-angle-right'
+	let ico = 'ico-angle-right'
+	let isPage = (string, page = props.page) => page === string ? ' js-selected' : ''
+	
 	return (
 		<nav className="nav">
-			<Link to="/" className={ page == 'home' ? selected : '' }>Startseite</Link>
-			<Link to="/customers" className={ page == 'customers' ? selected : '' }>Kunden</Link>
-			<Link to="/orders" className={ page == 'orders' ? selected : '' }>Bestellungen</Link>
-			<Link to="/products" className={ page == 'products' ? selected : '' }>Produkte</Link>
-			<Link to="/options" className={ page == 'options' ? selected : '' }>Einstellungen</Link>
-			<Link to="/protocol" className={ page == 'protocol' ? selected : '' }>Protokoll</Link>
+			<Link to="/" className={ ico + isPage('home') }>Startseite</Link>
+			<Link to="/customers" className={ ico + isPage('customers') }>Kunden</Link>
+			<Link to="/orders" className={ ico + isPage('orders') }>Bestellungen</Link>
+			<Link to="/products" className={ ico + isPage('products') }>Produkte</Link>
+			<Link to="/options" className={ ico + isPage('options') }>Einstellungen</Link>
+			<Link to="/protocol" className={ ico + isPage('protocol') }>Protokoll</Link>
 			<Link to="/#" className="logout">Logout</Link>
 		</nav>
 	)
