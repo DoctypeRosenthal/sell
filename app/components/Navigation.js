6777bdc4ca5faee0a2339edb3b68ef6b2df86cf9
lorenz <lorenz.rosenthal@gmail.com>
2016-03-10 19:38:44 +0100

    navigation :: now the dropdown-angle always appears next to the page title and better tells the user that there is a dropdown menu hidden

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -3,17 +3,17 @@ import React from 'react'
 import { Link } from 'react-router'
 
 const Navigation = (props) => {
-	let ico = ''
-	let isPage = (string, page = props.page) => page === string ? ' js-selected' : ''
+	const ico = () => <i className="ico-angle-down"></i>
+	const selectState = (string, page = props.page) => page === string ? ' js-selected' : ''
 	
 	return (
 		<nav className="nav">
-			<Link to="/" className={ ico + isPage('home') }>Startseite</Link>
-			<Link to="/customers" className={ ico + isPage('customers') }>Kunden</Link>
-			<Link to="/orders" className={ ico + isPage('orders') }>Bestellungen</Link>
-			<Link to="/products" className={ ico + isPage('products') }>Produkte</Link>
-			<Link to="/options" className={ ico + isPage('options') }>Einstellungen</Link>
-			<Link to="/protocol" className={ ico + isPage('protocol') }>Protokoll</Link>
+			<Link to="/" className={ selectState('home') }>Start{ ico() }</Link>
+			<Link to="/customers" className={ selectState('customers') }>Kunden{ ico() }</Link>
+			<Link to="/orders" className={ selectState('orders') }>Bestellungen{ ico() }</Link>
+			<Link to="/products" className={ selectState('products') }>Produkte{ ico() }</Link>
+			<Link to="/options" className={ selectState('options') }>Einstellungen{ ico() }</Link>
+			<Link to="/protocol" className={ selectState('protocol') }>Protokoll{ ico() }</Link>
 			<Link to="/#" className="logout">Logout</Link>
 		</nav>
 	)
